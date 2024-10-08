a7c42c7bfbf7eb3c62ac85b144f9c0baedcf8ca9
Lucas Marcondes <lucasnascimento95@gmail.com>
2024-10-08 11:32:24 -0300

    feat: add argparser
    Signed-off-by: Lucas Marcondes <lucasnascimento95@gmail.com>

---
+++ ofx_converter/argparser.py
@@ -0,0 +1,14 @@
+from argparse import ArgumentParser
+
+def get_main_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+            prog="ofx-converter",
+            description="Convert csv to OFX",
+            )
+    parser.add_argument(
+            "--account",
+            action="store",
+            type=str,
+            required=True
+            )
+    return parser
