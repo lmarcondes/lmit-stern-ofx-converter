2d66db8a3f8d96217240c922ae47911f8e85b632
Lucas Marcondes <lucasnascimento95@gmail.com>
2025-02-09 13:57:57 -0300

    fix: update settings
    Signed-off-by: Lucas Marcondes <lucasnascimento95@gmail.com>

--- ofx_converter/config.py
+++ ofx_converter/config.py
@@ -1,7 +1,7 @@
 from dynaconf import Dynaconf
 
 def get_settings():
-    settings = Dynaconf(settings_files=["settings.yaml"], environments=True)
-    return settings
+    settings = Dynaconf(use_dotenv=True, environments=True)
+    return settings["converter"]
 
 settings = get_settings()
