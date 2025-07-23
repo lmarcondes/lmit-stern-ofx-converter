d1e0529565e0c40b0d01721145e480523c393795
Lucas Marcondes <lucasnascimento95@gmail.com>
2025-07-23 09:25:08 -0300

    feat: implement return of output path
    Signed-off-by: Lucas Marcondes <lucasnascimento95@gmail.com>

--- tests/test_full_parsing.py
+++ tests/test_full_parsing.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from typing import Callable
 
+from ofxparse import OfxParser
+
 from ofx_converter.__main__ import filter_files_with_dates, run_account_parsing
 from ofx_converter.parsing.account import Account
 from ofx_converter.parsing.account_config import AccountConfig
@@ -9,15 +11,29 @@ from tests.base_test_case import BaseTestCase
 
 class TestFullConversion(BaseTestCase):
 
+    def setUp(self) -> None:
+        self._ofx_parser = OfxParser()
+
     def test_full_conversion_csv(self) -> None:
         account = Account("nubank-cartao")
         run_account_parsing(account)
+        paths = list(run_account_parsing(account))
+        for x in paths:
+            self.assertIsNotNone(x)
 
     def test_full_conversion_csv_with_dates(self) -> None:
         account = Account("nubank-cartao")
         parse_date: Callable[[str], datetime] = lambda dt: datetime.strptime(dt, "%Y-%m")
         from_date, to_date = parse_date("2025-03"), parse_date("2025-04")
-        run_account_parsing(account, from_date, to_date)
+        paths = list(run_account_parsing(account, from_date, to_date))
+        for path in paths:
+            self.assertIsNotNone(path)
+            if path is not None:
+                with open(path, 'rb') as of:
+                    ofx = self._ofx_parser.parse(of)
+                    of.close()
+                self.assertIsNotNone(ofx)
+
 
     def test_date_filter(self) -> None:
         account = Account("nubank-cartao")
